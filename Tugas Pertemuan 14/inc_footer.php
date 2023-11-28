2a0aaa6b213b7d4b818c52faef9b295eede1711e
Garrr <95403456+LigarArnata@users.noreply.github.com>
2023-11-28 09:35:40 +0700

    Add files via upload

---
+++ Tugas Pertemuan 14/inc_footer.php
@@ -0,0 +1,34 @@
+</div>
+
+    <div id="contact">
+        <div class="wrapper">
+            <div class="footer">
+                <div class="footer-section">
+                    <h3>CoDelicious.</h3>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sint, culpa!</p>
+                </div>
+                <div class="footer-section">
+                    <h3>About</h3>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sint, culpa!</p>
+                </div>
+                <div class="footer-section">
+                    <h3>Contact</h3>
+                    <p>Surabaya East Java</p>
+                    <p>Kode Pos: 60294</p>
+                </div>
+                <div class="footer-section">
+                    <h3>Social</h3>
+                    <p><b>YouTube: </b>CoDelicious</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div id="copyright">
+        <div class="wrapper">
+            &copy; 2020. <b>RumahRafif.</b> All Rights Reserved.
+        </div>
+    </div>
+    
+</body>
+</html>
---
+++ Tugas Pertemuan 14/inc_header.php
@@ -0,0 +1,30 @@
+<?php 
+include_once("inc/inc_koneksi.php");
+include_once("inc/inc_fungsi.php");
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Programming di CoDelicious.</title>
+    <link rel="stylesheet" href="<?php echo url_dasar()?>/css/style.css">
+    <link rel="stylesheet" type="text/css" href="styles.css" id="theme-style">
+</head>
+<body>
+    <nav>
+        <div class="wrapper">
+            <div class="logo"><a href='<?php echo url_dasar()?>'>CoDelicious.</a></div>
+            <div class="menu">
+                <ul>
+                    <li><a href="<?php echo url_dasar()?>#home">Home</a></li>
+                    <li><a href="<?php echo url_dasar()?>#courses">Courses</a></li>
+                    <li><a href="<?php echo url_dasar()?>#tutors">Tutors</a></li>
+                    <li><a href="<?php echo url_dasar()?>#partners">Partners</a></li>
+                    <li><a href="<?php echo url_dasar()?>#contact">Contact</a></li>
+                    <li><a href="" class="tbl-biru">Sign Up</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <div class="wrapper">
